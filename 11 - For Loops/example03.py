240a4d508979ae3e0ccca6089419a4e48d41408e
gyratory <gyratory@users.noreply.github.com>
2014-12-31 19:11:59 -0800

    Finished Chapter 11.

---
+++ 11 - For Loops/example03.py
@@ -0,0 +1,3 @@
+demolist = ['life', 42, 'the universe', 6, 'and', 7, 'everything']
+for item in demolist:
+    print("The current item is:", item)
---
+++ 11 - For Loops/example05.py
@@ -0,0 +1,7 @@
+list = [4, 5, 7, 8, 9, 1, 0, 7, 10]
+list.sort()
+prev = None
+for item in list:
+    if prev == item:
+        print("Duplicate of", prev, "found")
+    prev = item
